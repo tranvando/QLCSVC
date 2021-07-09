aadb07f99fb62eb5a896a35ec273dd9bf9513ab8
tranvando <trando1142000@gmail.com>
2021-07-09 09:52:26 +0700

    jPanelCacLoaiGD

--- src/DTO/jPanelCacLoaiGD.java
+++ src/DTO/jPanelCacLoaiGD.java
@@ -1,8 +1,62 @@
 
 package DTO;
-
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 public class jPanelCacLoaiGD {
-   
+    private JButton btn;
+    private JComboBox cbbMaTB, cbbTinhTrang;
+    private JTextField txtSL, txtDonGia,txtMoTa;
+
+    public JButton getBtn() {
+        return btn;
+    }
+
+    public void setBtn(JButton btn) {
+        this.btn = btn;
+    }
+
+    public JComboBox getCbbMaTB() {
+        return cbbMaTB;
+    }
+
+    public void setCbbMaTB(JComboBox cbbMaTB) {
+        this.cbbMaTB = cbbMaTB;
+    }
+
+    public JComboBox getCbbTinhTrang() {
+        return cbbTinhTrang;
+    }
+
+    public void setCbbTinhTrang(JComboBox cbbTinhTrang) {
+        this.cbbTinhTrang = cbbTinhTrang;
+    }
+
+    public JTextField getTxtSL() {
+        return txtSL;
+    }
+
+    public void setTxtSL(JTextField txtSL) {
+        this.txtSL = txtSL;
+    }
+
+    public JTextField getTxtDonGia() {
+        return txtDonGia;
+    }
+
+    public void setTxtDonGia(JTextField txtDonGia) {
+        this.txtDonGia = txtDonGia;
+    }
+
+    public JTextField getTxtMoTa() {
+        return txtMoTa;
+    }
+
+    public void setTxtMoTa(JTextField txtMoTa) {
+        this.txtMoTa = txtMoTa;
+    }
+    
     
     
    
