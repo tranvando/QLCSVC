314e8a4c461599bc5277ae615c84dce72ac06087
tranvando <trando1142000@gmail.com>
2021-07-09 09:49:33 +0700

    update

--- src/DTO/ThietBi.java
+++ src/DTO/ThietBi.java
@@ -1,10 +1,79 @@
-
 package DTO;
 
+import java.util.Objects;
 
 
 public class ThietBi {
-  
+    protected String MaTB, TenTB, MoTa, CauHinh;
+    protected int DonGia;
+
+    public String getMaTB() {
+        return MaTB;
+    }
+
+    public void setMaTB(String MaTB) {
+        this.MaTB = MaTB;
+    }
+
+    public String getTenTB() {
+        return TenTB;
+    }
+
+    public void setTenTB(String TenTB) {
+        this.TenTB = TenTB;
+    }
+
+    public String getMoTa() {
+        return MoTa;
+    }
+
+    public void setMoTa(String MoTa) {
+        this.MoTa = MoTa;
+    }
+
+    public String getCauHinh() {
+        return CauHinh;
+    }
+
+    public void setCauHinh(String CauHinh) {
+        this.CauHinh = CauHinh;
+    }
+
+    public int getDonGia() {
+        return DonGia;
+    }
+
+    public void setDonGia(int DonGia) {
+        this.DonGia = DonGia;
+    }
+
+   
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.MaTB);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ThietBi other = (ThietBi) obj;
+        if (!Objects.equals(this.MaTB, other.MaTB)) {
+            return false;
+        }
+        return true;
+    }
+    
     
     
 }
