c4aaff890b1fec9c7ae86ea62468dc51faedc7fb
vu-the-long <love01656354368@gmail.com>
2021-07-12 22:10:20 +0700

    add tablePhong

--- src/CustomTable/TablePhong.java
+++ src/CustomTable/TablePhong.java
@@ -1,8 +1,48 @@
 
 package CustomTable;
 
+import DTO.PhongDTO;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
 
 
-public class TablePhong{
+public class TablePhong extends AbstractTableModel{
+    private String name[]={"Mã phòng","Tên phòng"};
+    private Class classes[]={String.class,String.class};
+    ArrayList<PhongDTO> listPhong=new ArrayList<PhongDTO>();
+    public TablePhong(ArrayList<PhongDTO> listPhong)
+    {
+        this.listPhong=listPhong;
+    }
     
+    @Override
+    public int getRowCount() {
+        return listPhong.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return name.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+       switch(columnIndex)
+       {
+           case 0: return listPhong.get(rowIndex).getMaPhong();
+           case 1: return listPhong.get(rowIndex).getTenPhong();
+           
+           default: return null;
+       }
+    }
+
+    @Override
+    public Class getColumnClass(int columnIndex) {
+        return classes[columnIndex];
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return name[column];
+    }
 }
